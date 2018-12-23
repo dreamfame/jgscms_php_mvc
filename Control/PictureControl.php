e54cbf35a122d985e3177300c704dbb5f616d4a8
dreamfame <liuliuonlai@163.com>
2018-12-23 10:54:52 +0800

    1.增加明信片图片上传接口 2.增加明信片图片存放目录

--- Control/PictureControl.php
+++ Control/PictureControl.php
@@ -9,6 +9,9 @@
                 case "uploadImg":
                     PictureControl::AcceptImg();
                     break;
+                case "postcard":
+                    PictureControl::UploadPostCard();
+                    break;
             }
         }
 
@@ -45,6 +48,42 @@
                     $result['msg'] = "文件类型错误";
 				}
 			}
+            echo json_encode($result,JSON_UNESCAPED_UNICODE);
+        }
+
+        public function UploadPostCard()
+        {
+            $data = array('src'=>'','title'=>'');
+            $result = array('code' => 1, 'msg' => '服务器故障', 'data' => $data);
+            if($_FILES["file"]["error"])
+            {
+                $result['msg'] = $_FILES["file"]["error"];
+            }
+            else{
+                if(($_FILES["file"]["type"]=="image/png"||$_FILES["file"]["type"]=="image/jpeg")&&$_FILES["file"]["size"]<1024000)
+                {
+                    $filename ="../View/images/postcard/".time().$_FILES["file"]["name"];
+                    $src = "/images/postcard/".time().$_FILES["file"]["name"];
+                    $filename =iconv("UTF-8","gb2312",$filename);
+                    //检查文件或目录是否存在
+                    if(file_exists($filename))
+                    {
+                        $result['msg'] = "文件已存在";
+                    }
+                    else
+                    {
+                        //保存文件,   move_uploaded_file 将上传的文件移动到新位置
+                        move_uploaded_file($_FILES["file"]["tmp_name"],$filename);//将临时地址移动到指定地址
+                        $result['code'] = 0;
+                        $result['msg'] = "上传成功";
+                        $data['src'] = $src;
+                        $result['data'] = $data;
+                    }
+                }
+                else{
+                    $result['msg'] = "文件类型错误";
+                }
+            }
             echo json_encode($result,JSON_UNESCAPED_UNICODE);
         }
     }
