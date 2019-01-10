c7883316b0f7b91bfa28f357d5d7155df92c67ae
dreamfame <liuliuonlai@163.com>
2019-01-10 23:26:05 +0800

    图片合成方法

--- Control/TestControl.php
+++ Control/TestControl.php
@@ -3,31 +3,12 @@
  * Created by PhpStorm.
  * User: Liu Liu
  */
-require_once '../Extensions/ImageCompress.php';
+require_once '../Extensions/ImageMerge.php';
 header("Content-Type: text/html;charset=utf-8");
 Class TestControl
 {
     public function Test(){
-        $path = '../View/images/scenicImgs';///当前目录
-        $handle = opendir($path); //当前目录
-        while (false !== ($file = readdir($handle))) { //遍历该php文件所在目录
-
-            list($filesname,$kzm)=explode(".",$file);//获取扩展名
-
-            if($kzm=="gif" or $kzm=="jpg" or $kzm=="JPG" or $kzm=="png") { //文件过滤
-
-                if (!is_dir('./'.$file)) { //文件夹过滤
-
-                    $array[]=$file;//把符合条件的文件名存入数组
-                    $source =  $path."/".$file;//原图片名称
-                    $dst_img = $path."/".$file;//压缩后图片的名称
-                    $percent = 0.5;  #原图压缩，不缩放，但体积大大降低
-                    $image = (new ImageCompress($source,$percent))->compressImg($dst_img);
-                }
-
-            }
-
-        }
-
+        echo "da";
+        ImageMerge::MergePic();
     }
 }
--- Extensions/ImageMerge.php
+++ Extensions/ImageMerge.php
@@ -7,19 +7,20 @@
  */
 
 Class ImageMerge{
-    static public function MergePic(){
+    static public function MergeText(){
 
     }
 
-    static public function MergeText(){
-        $bigImgPath = '../View/images/1545309416default.png';
-        $qCodePath = '../View/images/1545309636alipay.jpg';
-
+    static public function MergePic(){
+        $bigImgPath = '../View/images/a.png';
+        $qCodePath = '../View/images/c.png';
+        $qCodePath = ImageMerge::resize($qCodePath,260,340);
+        $image = "../View/images/1.png";
         $bigImg = imagecreatefromstring(file_get_contents($bigImgPath));
         $qCodeImg = imagecreatefromstring(file_get_contents($qCodePath));
 
         list($qCodeWidth, $qCodeHight, $qCodeType) = getimagesize($qCodePath);
-        imagecopymerge($bigImg, $qCodeImg, 200, 300, 0, 0, $qCodeWidth, $qCodeHight, 100);
+        imagecopymerge($bigImg, $qCodeImg, 145, 200, 0, 0, 260, 340, 100);
 
         list($bigWidth, $bigHight, $bigType) = getimagesize($bigImgPath);
 
@@ -27,15 +28,15 @@ Class ImageMerge{
         switch ($bigType) {
             case 1: //gif
                 header('Content-Type:image/gif');
-                imagegif($bigImg);
+                imagegif($bigImg,$image);
                 break;
             case 2: //jpg
                 header('Content-Type:image/jpg');
-                imagejpeg($bigImg);
+                imagejpeg($bigImg,$image);
                 break;
             case 3: //jpg
                 header('Content-Type:image/png');
-                imagepng($bigImg);
+                imagepng($bigImg,$image);
                 break;
             default:
                 # code...
@@ -44,5 +45,59 @@ Class ImageMerge{
 
         imagedestroy($bigImg);
         imagedestroy($qCodeImg);
+    }
+
+    static function resize($src, $width_value=535, $height_value=400) {
+        $temp = pathinfo($src);
+        $filename = time().'.jpg';
+        $dir = "../View/images/";    //文件所在的文件夹
+        $savepath = $dir."new".$filename; //缩略图保存路径
+        //获取图片的基本信息
+        $info = getimagesize($src);
+        $width = $info[0];      //获取图片宽度
+        $height = $info[1];     //获取图片高度
+        if(($width/$height) >= ($width_value/$height_value)){ //宽度优先
+            $w_mid = $width_value;						  //压缩后图片的宽度
+            $h_mid = intval($width_value * $height/$width);//等比缩放图片高度
+            $mid_x = 0;
+            $mid_y = intval(($height_value-$h_mid)/2);
+        }else{											//高度优先
+            $w_mid = intval($height_value * $width/$height);							//压缩后图片的宽度
+            $h_mid = $height_value;//等比缩放图片高度
+            $mid_x = intval(($width_value-$w_mid)/2);
+            $mid_y = 0;
+        }
+
+        $temp_img = imagecreatetruecolor($width_value , $height_value);		//创建画布
+        $white = imagecolorallocate($temp_img, 255, 255, 255);
+        imagefill($temp_img, 0, 0, $white);
+        $im = ImageMerge::create($src);
+        imagecopyresampled($temp_img, $im, $mid_x, $mid_y, 0, 0, $w_mid, $h_mid, $width, $height);
+        imagejpeg($temp_img,$savepath, 100);
+        imagedestroy($im);
+
+        return $savepath;
+    }
+
+    /**
+     * 创建图片，返回资源类型
+     * @param  string $src 图片路径
+     * @return resource $im 返回资源类型
+     */
+    static function create($src)
+    {
+        $info = getimagesize($src);
+        switch ($info[2]) {
+            case 1:
+                $im = imagecreatefromgif($src);
+                break;
+            case 2:
+                $im = imagecreatefromjpeg($src);
+                break;
+            case 3:
+                $im = imagecreatefrompng($src);
+                break;
+        }
+        return $im;
     }
 }
